e10dfa1e740ba36da942dabf92252ffbea4d06fd
xuligang <429795986@qq.com>
2019-10-23 14:57:54 +0800

    数据结构和算法

---
+++ src/main/java/com/lg/Application.java
@@ -0,0 +1,18 @@
+package com.lg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 数据结构和算法学习
+ *
+ * @author Xulg
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
